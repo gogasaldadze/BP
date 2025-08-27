d633b35740db54ca9cd0711558762df2ea8657ab
giorgi <saldadzegoga@gmail.com>
2025-08-27 08:36:53 +0400

    abstractmodel creation

---
+++ common/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+
+class AbstractModel(models.Model):
+
+    uuid = models.UUIDField(uuid=uuid.uuid4, editable=False, unique=True, db_index=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
