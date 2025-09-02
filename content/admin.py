a8c8284f3b18fbc9e08a6b796ae2fd49291bc675
giorgi <saldadzegoga@gmail.com>
2025-09-02 22:00:17 +0400

    basic admin

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ["name", "category"]
+    readonly_fields = ["id", "uuid", "created_at", "updated_at"]
+    list_display = [
+        "name",
+        "category",
+        "created_at",
+    ]
